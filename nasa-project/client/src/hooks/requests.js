bdeeda7cbf12012bd8860ca6588ceec135dca03d
Samuel Alves <110740160+samuel-alves21@users.noreply.github.com>
2023-11-06 12:59:10 -0300

    setting the firts route

---
+++ nasa-project/client/src/hooks/requests.js
@@ -0,0 +1,28 @@
+const API_URL = 'http://localhost:5000'
+
+async function httpGetPlanets() {
+  const data = await fetch(`${API_URL}/planets`).then((res) => res.json());
+  console.log(data)
+}
+
+async function httpGetLaunches() {
+  // TODO: Once API is ready.
+  // Load launches, sort by flight number, and return as JSON.
+}
+
+async function httpSubmitLaunch(launch) {
+  // TODO: Once API is ready.
+  // Submit given launch data to launch system.
+}
+
+async function httpAbortLaunch(id) {
+  // TODO: Once API is ready.
+  // Delete launch with given ID.
+}
+
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
--- nasa-project/server/src/app.js
+++ nasa-project/server/src/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
 
+const { planetsRouter } = require('./routes/planets/planets.router')
+
 const app = express()
 app.use(express.json())
+
+app.use('/planets', planetsRouter)
+
 
 module.exports = {
   app
