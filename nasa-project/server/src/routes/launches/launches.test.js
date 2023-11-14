b9b5bfe7bc7db8a3a69f4a43a48750e982d3ee4e
Samuel Alves <110740160+samuel-alves21@users.noreply.github.com>
2023-11-14 12:50:31 -0300

    starting with tests with jest

---
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -0,0 +1,6 @@
+describe("GET /planets", () => {
+  test('it should return the planets collection with a 200 status code', () => {
+    const response = 200
+    expect(response).toBe(200)
+  })
+})
